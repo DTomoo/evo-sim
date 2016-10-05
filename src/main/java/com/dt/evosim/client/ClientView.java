b49a30b50528bacb49d79882ecc80d9ed56d0c21
DTomoo <tamas.danyi@gmail.com>
2016-10-05 18:11:19 +0200

    fixing client view

--- src/main/java/com/dt/evosim/client/ClientView.java
+++ src/main/java/com/dt/evosim/client/ClientView.java
@@ -2,56 +2,127 @@ package com.dt.evosim.client;
 
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
+import com.dt.evosim.domain.SimObj;
+import com.dt.evosim.simulation.Environment;
+import com.dt.evosim.simulation.Simulation;
+import com.dt.evosim.simulation.SimulationState;
+import com.dt.physics.common.Position;
+
 public class ClientView extends JFrame {
 
+  private static final int TOP_BOTTOM_BORDER = 30;
+  private static final int SIDE_BORDER = 7;
   private static final long serialVersionUID = 1L;
-  private int objSize;
-  private final int width;
-  private final int height;
-  private JFrame frame;
-  private Canvas canvas;
 
-  public ClientView(int w, int h, int objSize) {
-    this.width = w;
-    this.height = h;
-    this.objSize = objSize;
+  /** width of the window */
+  private final int frameWidth;
+  /** height of the window */
+  private final int frameHeight;
+
+  /** width of the drawing view space */
+  private final int viewWidth;
+  /** height of the drawing view space */
+  private final int viewHeight;
+
+  /** border size out of the drawing view space */
+  private final int borderSize;
+  /** canvas on the window */
+  private Canvas canvas;
+  private Graphics graphics;
+  private BufferStrategy bufferStrategy;
+
+  public ClientView(int w, int h, int borderSize) {
+    this.viewWidth = w;
+    this.viewHeight = h;
+    this.frameWidth = w + 2 * borderSize;
+    this.frameHeight = h + 2 * borderSize;
+    this.borderSize = borderSize;
   }
 
-  private JFrame createJFrame() {
+  public void displaySimulation(Simulation simulation) {
+    resetGraphics(simulation);
+    drawObjects(simulation.getSimulationState());
+    simulation.getSimulationState().getSimulationObjects().forEach(this::drawCircle);
+    flushDrawing();
+  }
+
+  private void initJFrame() {
     final String title = "Test Window";
-    JFrame frame = new JFrame(title);
-    frame.setSize(width, height);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setLocationRelativeTo(null);
-    frame.setResizable(false);
-    frame.setVisible(true);
-    return frame;
+    this.setTitle(title);
+    this.setSize(frameWidth + SIDE_BORDER, frameHeight + TOP_BOTTOM_BORDER);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setLocationRelativeTo(null);
+    this.setResizable(false);
+    this.setVisible(true);
   }
 
   private Canvas createCanvas() {
     Canvas canvas = new Canvas();
-    canvas.setSize(width, height);
-    canvas.setBackground(Color.BLACK);
+    canvas.setSize(frameWidth, frameHeight);
+    canvas.setBackground(Color.GRAY);
     canvas.setVisible(true);
     canvas.setFocusable(false);
     return canvas;
   }
 
   public void init() {
-    frame = createJFrame();
+    initJFrame();
     canvas = createCanvas();
-    frame.add(canvas);
     canvas.createBufferStrategy(1);
+    bufferStrategy = canvas.getBufferStrategy();
+
+    this.add(canvas);
+  }
+
+  private Position getViewPosition(Position pos) {
+    return new Position(pos.getX() + borderSize, pos.getY() + borderSize);
   }
 
   public Canvas getCanvas() {
     return canvas;
   }
 
-  public int getObjSize() {
-    return objSize;
+  public int getBorderSize() {
+    return borderSize;
+  }
+
+  public int getViewHeight() {
+    return viewHeight;
+  }
+
+  public int getViewWidth() {
+    return viewWidth;
+  }
+
+  private void resetGraphics(Simulation simulation) {
+    graphics = bufferStrategy.getDrawGraphics();
+    Environment env = simulation.getEnvironment();
+    int x = env.getMinWidth() + borderSize;
+    int y = env.getMinHeight() + borderSize;
+    int w = env.getMaxWidth() - env.getMinWidth() + 1;
+    int h = env.getMaxHeight() - env.getMinHeight() + 1;
+    graphics.clearRect(x, y, w, h);
+    graphics.setColor(Color.GREEN);
+    graphics.drawRect(borderSize, borderSize, viewWidth, viewHeight);
+  }
+
+  private void drawCircle(SimObj simObj) {
+    int size = simObj.getSize();
+    Position drawPos = getViewPosition(simObj.getPosition());
+    graphics.drawOval(drawPos.getX() - size, drawPos.getY() - size, 2 * size, 2 * size);
+  }
+
+  private void flushDrawing() {
+    bufferStrategy.show();
+    graphics.dispose();
+  }
+
+  private void drawObjects(SimulationState simulationState) {
+    simulationState.getSimulationObjects().forEach(this::drawCircle);
   }
 }
--- src/main/java/com/dt/evosim/client/Program.java
+++ src/main/java/com/dt/evosim/client/Program.java
@@ -10,25 +10,25 @@ public class Program {
 
   public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
     final int width = 200;
-    final int height = 200;
-    //
+    final int height = 500;
+    final int border = 30;
+
     Simulation clientModel = createModel(width, height);
-    ClientView clientView = createView(width + 50, height + 50, 5);
-    //
-    //
+    ClientView clientView = createView(width, height, border);
+
     ClientController controller = new ClientController(clientModel, clientView);
     controller.startSim();
   }
 
-  private static ClientView createView(final int width, final int height, final int objSize) {
-    ClientView clientView = new ClientView(width, height, objSize);
+  private static ClientView createView(final int width, final int height, int borderSize) {
+    ClientView clientView = new ClientView(width, height, borderSize);
     clientView.init();
     return clientView;
   }
 
   private static Simulation createModel(int width, int height) {
     SimulationBuilder sb = new SimulationBuilder(width, height);
-    Simulation clientModel = sb.build(5);
+    Simulation clientModel = sb.build(1);
     return clientModel;
   }
 }
